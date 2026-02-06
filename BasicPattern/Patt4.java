c157154734d44775b4ef61e49caec31b3947ee26
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2026-02-07 00:57:26 +0530

    added

--- BasicPattern/Patt4.java
+++ BasicPattern/Patt4.java
@@ -1,6 +1,10 @@
 package BasicPattern;
 import java.util.Scanner;
-
+// *****
+// ****
+// ***
+// **
+// *
 public class Patt4 {
     public static void main(String[] args) {
         System.out.println("Enter the Size of the Box:");
